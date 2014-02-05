39b82fc7cebf0097247d707baaca05073d622600
Ryan Stout <rstout@andrew.cmu.edu>
2014-02-05 00:48:23 -0800

    Notes update

--- parst/Token.rs
+++ parst/Token.rs
@@ -9,6 +9,50 @@ struct Token<T> {
     lex: ~LexCmd
 }
 
+// Some of these functions should be defined in terms of the others, and
+// can thus be the same for all LexCmds
+struct LexCmd {
+    val: ~str,
+    done: bool,
+    accepts: fn(char) -> bool,
+    // Can implement many and many1 here: if self.accepts, return self with
+    // updated val, else return next LexCmd in the chain
+    consume: fn(&self, char) -> Option<~LexCmd>,
+    // Use if we've consumed something and we're not done
+    get_next: fn(&self) -> ~LexCmd,
+    // Used for (>>) ?
+    set_next: fn(&self)
+}
+
+fn symbol(c: char) -> ~LexCmd {
+    // use closure here
+    fn accepts(c2: char) -> bool {
+        if c == c2 {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn consume(self: ~LexCmd, c: char) -> Option<~LexCmd> {
+        if self.accepts(c) {
+            Some(LexCmd { val: c, done: true,
+                          accepts: fn (c: char) -> bool { false },
+                          consume: fn(&self, char) -> Option<~LexCmd>
+                          { None }
+                })
+        } else {
+            None
+        }
+    }
+}
+
+/*
+fn: >> (~LexCmd) -> ~LexCmd {
+
+}
+*/
+
 impl<T> Token<T> {
     fn new(k: ~T, lex: ~LexCmd) -> Token<T> {
         Token { kind: k, val: ~"", lex: lex }
@@ -22,9 +66,10 @@ enum MyTokenType {
 }
 
 struct Lexer<T> {
-
+    kind: T
 }
 
+/*
 impl<T> Lexer<T> {
     fn new(dict: ~Hashmap<LexCmd, T>) -> Lexer<T> {
         // undefined
@@ -34,6 +79,7 @@ impl<T> Lexer<T> {
 
     }
 }
+*/
 
 /*
 fn getGeneric<T: Token<T>>(a: T) -> ~str {
@@ -42,14 +88,14 @@ fn getGeneric<T: Token<T>>(a: T) -> ~str {
 */
 
 // under the hood
+/*
 struct LexCmd {
-    /*
     state: ~State;
     accept: fn; // update DFA?
     dfa: DFA; // Need way combine DFAs/LexCmds
-    */
     kind: int
 }
+*/
 
 fn main() {
     //let t: Token<MyTokenType> = Token { kind: Plus, value: ~"" };
