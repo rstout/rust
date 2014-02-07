7a4a4f97cdd9932dd24a6749cbe2c123d1bf44a6
Ryan Stout <rstout@andrew.cmu.edu>
2014-02-07 08:58:37 -0800

    Updated notes, Lexer.rs

--- parst/Lexer.rs
+++ parst/Lexer.rs
@@ -9,12 +9,45 @@ enum C0Tokens {
     Digit
 }
 
+struct HasClosure<'a> {
+    closure: 'a |int| -> bool
+}
+
+fn new_eq_check<'a>(i: int) -> ~HasClosure<'a> {
+    ~HasClosure { closure: |j| { i == j } }
+}
+
 /*
-fn symbol(c: char) -> ~LexCmd {
-    ~LexCmd
+fn symbol<'a>(sym: char) -> ~LexCmd<'a> {
+    ~LexCmd {
+        kind: Symbol,
+        accepts: |c| { sym == c },
+        modifier: None
+    }
+}
+
+// Is this needed? Doesn't having an accepts function make this obsolete?
+enum LexKind {
+    Symbol,
+    OneOf,
+    Range
+}
+
+// How do we handle multiple modifiers?
+enum LexModifier {
+    Many,
+    Many1,
+    Not
+}
+
+struct LexCmd<'a> {
+    kind: LexKind,
+    accepts: 'a |char| -> bool, // maybe a closure
+    modifier: Option<LexModifier>
 }
 */
 
+/*
 // (>>) sets the next field
 struct LexCmd {
     val: ~str,
@@ -27,7 +60,9 @@ struct LexCmd {
     consume: fn(char) -> Option<~LexCmd>,
     next: Option<~LexCmd>
 }
+*/
 
+/*
 // What is "returned" by the Lexer
 struct Token<T> {
     kind: T,
@@ -40,12 +75,12 @@ impl<T> Token<T> {
     }
 }
 
-struct Lexer<T> {
-    cmds: ~[(~LexCmd, T)]
+struct Lexer<'a, T> {
+    cmds: ~[(~LexCmd<'a>, T)]
 }
 
-impl<T> Lexer<T> {
-    fn new(cmds: ~[(~LexCmd, T)]) -> ~Lexer<T> {
+impl<'a, T> Lexer<'a, T> {
+    fn new(cmds: ~[(~LexCmd<'a>, T)]) -> ~Lexer<'a, T> {
         ~Lexer { cmds: cmds }
     }
 
@@ -55,3 +90,4 @@ impl<T> Lexer<T> {
         ~[]
     }
 }
+*/
