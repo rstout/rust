120638476466599683ec4cb717b0c214a3cea0ad
Ryan Stout <rstout@mbp-001085.ad.whirl.net>
2014-02-12 18:25:32 -0800

    update notes

--- parst/dfa.rs
+++ parst/dfa.rs
@@ -5,15 +5,50 @@ fn main() {}
 
 // add functions for querying state
 // make a trait?
-trait DFA<T> {
-    consume: fn(T)
+trait DFA<T, U> {
+    fn consume(T);
+    fn get_start_node() -> ~Node<T, U>;
+    fn get_current_node() -> ~Node<T, U>;
 }
 
 
+// T is input type, U is metadata type
+// We can save Node links in a struct that impls this trait, and then
+// fn transition() can case on its input an return one of the saved Node links
+trait Node<T, U> {
+    fn transition(&self, T) -> ~Node<T, U>;
+    fn get_metadata(&self) -> Option<U>;
+    fn is_accept(&self) -> bool;
+}
 
 
+// Should be put in lexer.rs
+struct LexNode<T> {
+    token_kind: T,
+    is_accept: bool, // Is an accepting state
+    lex_cmd: ~LexCmd, // Has an accept() function
+    success_node: ~LexNode<T>,
+    fail_node: ~LexNode<T> // Make an option instead of start node?
+}
 
+impl<T> Node<char, T> for LexNode<T> {
+    fn transition(&self, c: char) -> ~LexNode<T> {
+        if self.lex_cmd.accept(c) {
+            // But this is an owned pointer, can we hand it out?
+            self.success_node
+        } else {
+            self.fail_node
+        }
+    }
 
+    fn get_metadata(&self) -> Option<T> {
+        Some(self.token_kind)
+    }
+
+    fn is_accept(&self) -> bool {
+        self.is_accept
+    }
+}
 
 
 
